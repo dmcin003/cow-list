3189db7f7d7b7133eeb1bb9133e66e3fca86cda3
dmcin003 <dmcin003@fiu.edu>
2021-10-19 16:08:29 -0400

    Added Update Form

--- client/src/Cow.js
+++ client/src/Cow.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import UpdateForm from './UpdateForm.js';
 
  function Cow(props){
 
 
 
-  return (<div onClick={()=>{props.setCow(props.cow)}}>{props.cow.name}</div>)
+  return (<div onClick={()=>{props.setCow(props.cow)}}>{props.cow.name}
+  <div></div>
+  <UpdateForm oldCow={props.cow}/>
+  </div>)
 
 }
 
