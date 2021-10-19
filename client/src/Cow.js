022c8b6bde43b84a91d347c342e4031d2cabe118
dmcin003 <dmcin003@fiu.edu>
2021-10-19 10:06:07 -0400

    added onClick to each cow

--- client/src/Cow.js
+++ client/src/Cow.js
@@ -2,8 +2,11 @@ import React from 'react';
 
  function Cow(props){
   console.log(props);
+  function handleClick(event){
+    console.log('I was clicked');
+  }
 
-  return (<div>{props.cow.name}</div>)
+  return (<div onClick={handleClick}>{props.cow.name}</div>)
 
 }
 
