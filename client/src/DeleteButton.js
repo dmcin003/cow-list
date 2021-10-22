ea43368d5427148801bc65ce8e6d3e314976c81e
dmcin003 <dmcin003@fiu.edu>
2021-10-21 21:27:14 -0400

    changed axios's delete obj key to data so req.body in server will work

--- client/src/DeleteButton.js
+++ client/src/DeleteButton.js
@@ -8,7 +8,7 @@ let Delete = (props)=>{
     console.log('Hi I am a cow and I am going to be deleted');
     axios.delete('/api/cows',{data: props.cow})
       .then((response)=>{
-        console.log(response)
+        console.log(response);
         props.renderCows();
       })
       .catch((err)=>{
